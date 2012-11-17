d519d8f4c5ee77d695247e9dcb3d037a6dd07f76
Brendan Arnold <none@none>
2012-11-17 03:31:26 +0000

    Working on design

--- wp7/scripts/classify_glossary_links.py
+++ wp7/scripts/classify_glossary_links.py
@@ -1,9 +1,41 @@
+
+import os
 from bs4 import BeautifulSoup
 
+for fn in os.listdir("../"):
+    if not fn.endswith(".html"):
+        continue
 
-IN_FN = r'..\mvvm.html'
+    fh = open('..\\' + fn)
 
-soup = BeautifulSoup(open(IN_FN))
+    soup = BeautifulSoup(fh)
 
-la = soup.find_all('a')
-print la
+    for a in soup.find_all('a'):
+        if a.get('href') is None:
+            continue
+        if not a.get('href').startswith('glossary.html'):
+            continue
+        a["class"] = "glossaryterm"
+    out_fh = open('dump\\' + fn, 'w')
+    out_fh.write(soup.prettify())
+        
+
+
+# import elementtree.ElementTree as ET
+# import os
+
+
+
+#     fp = '..\\' + fn
+#     fh = open(fp)
+#     tree = ET.parse(fh)
+#     for a in tree.findall(".//a"):
+#         href = a.attrib.get("href")
+#         if href is None:
+#             continue
+#         if href.startswith("glossary.html"):
+#             a.attrib["class"] = "glossaryterm"
+#     out_fh = open("dump\\" + fn, "w")
+#     tree.write(out_fh)
+#     
+# 
